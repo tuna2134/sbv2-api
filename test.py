697d330e281a047c4a1d1e89d21bcf17fa6f4825
sanami <sanamiev@gmail.com>
2024-09-11 18:42:31 +0900

    update README.md

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 res = requests.post(
-    "http://localhost:3001/synthesize",
+    "http://localhost:3000/synthesize",
     json={"text": "おはようございます", "ident": "tsukuyomi"},
 )
 with open("output.wav", "wb") as f:
