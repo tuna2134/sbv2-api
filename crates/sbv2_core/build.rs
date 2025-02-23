e262694702bca9dac51b6a66b66e574f7a13f789
Googlefan <google@neody.dev>
2025-02-23 04:45:58 +0000

    fix

--- crates/sbv2_core/build.rs
+++ crates/sbv2_core/build.rs
@@ -1,12 +1,15 @@
-use std::env;
 use std::fs;
 use std::io::copy;
-use std::path::Path;
+
+use home_dir::HomeDirExt;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR")?;
-    let out_path = Path::new(&out_dir).join("all.bin");
+    let out_dir = "~/.cache/sbv2-api-assets".expand_home().unwrap();
+    let out_path = out_dir.join("all.bin");
     if !out_path.exists() {
+        if !out_dir.exists() {
+            fs::create_dir_all(out_dir).unwrap();
+        }
         println!("cargo:warning=Downloading dictionary file...");
         let mut response =
             ureq::get("https://huggingface.co/neody/sbv2-api-assets/resolve/main/dic/all.bin")
